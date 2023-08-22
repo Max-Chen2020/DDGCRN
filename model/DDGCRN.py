f08f2b7c7250f3c8c146656c1450ccce7f18882c
Haoyang Chen <63090484+Max-Chen2020@users.noreply.github.com>
2023-08-22 21:36:06 +1000

    Update DDGCRN.py

--- model/DDGCRN.py
+++ model/DDGCRN.py
@@ -96,8 +96,8 @@ class DDGCRN(nn.Module):
         if i == 1:
             init_state1 = self.encoder1.init_hidden(source.shape[0])  # [2,64,307,64] 前面是2是因为有两层GRU
             output, _ = self.encoder1(source, init_state1, node_embeddings)  # B, T, N, hidden
-            # output = output[:, -1:, :, :]                                   #B, 1, N, hidden
-            output = self.dropout1(output[:, -1:, :, :])
+                                  
+            output = self.dropout1(output[:, -1:, :, :]) #B, 1, N, hidden
 
             # CNN based predictor
             output1 = self.end_conv1(output)  # B, T*C, N, 1
@@ -107,8 +107,8 @@ class DDGCRN(nn.Module):
         else:
             init_state1 = self.encoder1.init_hidden(source.shape[0])   #[2,64,307,64] 前面是2是因为有两层GRU
             output, _ = self.encoder1(source, init_state1, node_embeddings)      #B, T, N, hidden
-            # output = output[:, -1:, :, :]                                   #B, 1, N, hidden
-            output = self.dropout1(output[:, -1:, :, :])
+                                              
+            output = self.dropout1(output[:, -1:, :, :]) #B, 1, N, hidden
 
             #CNN based predictor
             output1 = self.end_conv1(output)                        #B, T*C, N, 1
