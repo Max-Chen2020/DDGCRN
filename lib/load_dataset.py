0881630eb854d920c2b6a5244d35b34498cfd1b9
Haoyang Chen <63090484+Max-Chen2020@users.noreply.github.com>
2023-09-15 19:29:41 +1000

    Update load_dataset.py

--- lib/load_dataset.py
+++ lib/load_dataset.py
@@ -8,7 +8,7 @@ def load_st_dataset(dataset):
         data_path = os.path.join('./data/PEMS07(M)/V_228.csv')
         data = np.array(pd.read_csv(data_path,header=None)) 
     elif dataset == 'Melb':
-        data_path = os.path.join('./data/Melb.h5')
+        data_path = os.path.join('./data/melb.h5')
         # speed = pd.read_hdf(data_path, key = 'speed', mode='r')
         data = pd.read_hdf(data_path, key='flow', mode='r')
         # data = np.stack((flow, speed), axis=-1)
