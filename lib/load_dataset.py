c0d545d216927d9035420830c16c70d3e2eb9cbd
Haoyang Chen <63090484+Max-Chen2020@users.noreply.github.com>
2023-09-15 19:16:13 +1000

    Update load_dataset.py

--- lib/load_dataset.py
+++ lib/load_dataset.py
@@ -7,14 +7,11 @@ def load_st_dataset(dataset):
     if dataset == 'PEMSD7(M)':
         data_path = os.path.join('./data/PEMS07(M)/V_228.csv')
         data = np.array(pd.read_csv(data_path,header=None)) 
-    elif dataset == 'METR-LA':
-        data_path = os.path.join('./data/METR-LA/METR.h5')
-        data = pd.read_hdf(data_path)
-    elif dataset == 'PEMS':
-        data_path = os.path.join('./data/pems.h5')
-        speed = pd.read_hdf(data_path, key = 'speed', mode='r')
-        flow = pd.read_hdf(data_path, key='flow', mode='r')
-        data = np.stack((speed, flow), axis=-1)
+    elif dataset == 'Melb':
+        data_path = os.path.join('./data/Melb.h5')
+        # speed = pd.read_hdf(data_path, key = 'speed', mode='r')
+        data = pd.read_hdf(data_path, key='flow', mode='r')
+        # data = np.stack((flow, speed), axis=-1)
     else:
         raise ValueError
     if len(data.shape) == 2:
