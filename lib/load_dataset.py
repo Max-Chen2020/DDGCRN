9e145436ff67d12d2026866afc93015624f310e3
Haoyang Chen <63090484+Max-Chen2020@users.noreply.github.com>
2023-09-15 19:34:04 +1000

    Update load_dataset.py

--- lib/load_dataset.py
+++ lib/load_dataset.py
@@ -9,9 +9,9 @@ def load_st_dataset(dataset):
         data = np.array(pd.read_csv(data_path,header=None)) 
     elif dataset == 'Melb':
         data_path = os.path.join('./data/melb.h5')
-        # speed = pd.read_hdf(data_path, key = 'speed', mode='r')
-        data = pd.read_hdf(data_path, key='flow', mode='r')
-        # data = np.stack((flow, speed), axis=-1)
+        speed = pd.read_hdf(data_path, key = 'speed', mode='r')
+        flow = pd.read_hdf(data_path, key='flow', mode='r')
+        data = np.stack((flow, speed), axis=-1)
     else:
         raise ValueError
     if len(data.shape) == 2:
